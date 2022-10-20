67d38bd94a47af5fa94a6232d7f42a372e47e6ee
Patrick hachicho <Hachichopatrick@gmail.com>
2022-10-19 20:56:47 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // Import data
-
+import Gallery from "./components/Gallery.js"
+import Posting from "./components/Posting.js"
+import Sidebar from "./components/Sidebar.js"
+import Searchbar from "./components/Searchbar.js"
+import Directory from "./components/Directory.js"
+import Help from "./components/Help"
+import {postings} from "./postings.js"
 // Import components
 import './App.css';
 
 function App(){
   return (
     <div>
-      <h1>craigslist</h1>
+      <Gallery></Gallery>
+      <postings></postings>
+      <Posting></Posting>
+      <Searchbar></Searchbar>
+      <Help></Help>
+      <Directory></Directory>
+      <Sidebar></Sidebar>
+      <Gallery positngs = {postings}></Gallery>
       <div className="App">
         {/* Your content will go here! */}
       </div>
---
+++ src/components/Posting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Posting() {
+  return <h1>Posting</h1>;
+}
+
+export default Posting
